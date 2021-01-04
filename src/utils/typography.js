a46390ee9fe767e2341be7ba238006a65799429b
Rodolfo Dutra <rodolfo.borgesdutra@docyet.com>
2021-01-04 21:41:48 +0100

    remove google fonts. fixe post content image size in small screens.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,10 @@
 import Typography from "typography"
 import FairyGatesTheme from "typography-theme-fairy-gates"
 
+FairyGatesTheme.headerFontFamily = ['Verdana', 'sans-serif']
+FairyGatesTheme.bodyFontFamily = ['Verdana' , 'sans-serif']
+FairyGatesTheme.googleFonts = []
+
 const typography = new Typography(FairyGatesTheme)
 
 export default typography
