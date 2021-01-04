7ac14432f0581ad90f5ee41c42554c429b606290
Rodolfo Dutra <rodolfo.borgesdutra@docyet.com>
2021-01-04 21:50:28 +0100

    rewrite some sentences in the about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,21 +16,20 @@ const AboutPage = () => (
       My first passion in tech was aircrafts however, destiny brought me to work
       in the web as a front-end developer.
     </p>
-    <p>
-      I love reading José Saramago, I am caught up listening to Jacob Collier
-      and every day I believe more in the power of our inner energy.
-    </p>
+
     <h3>Cookies & Data Privacy</h3>
+    
     <p>
-      This page does not make use of any cookies. This page does not gather or
+      This page does not make use of cookies. This page does not gather or
       process your personal information. Pages linked may have their own privacy
-      agreements and cookies, be sure to inform yourself about these when
+      agreements and cookies, be sure to inform about these when
       visiting them.
     </p>
     <p>
-      Source code of this page can be found{" "}
-      <a href="https://github.com/deer-rodolfo/deer-blog">here</a>. Any
-      anormality you detect feel free to <Link to="/contact">contact me</Link>.
+      Source code of this page can be found&nbsp;
+      <a href="https://github.com/deer-rodolfo/deer-blog">here</a>.
+
+      Feel free to <Link to="/contact">contact me</Link>.
     </p>
   </Layout>
 )
